c8ccccc352828beed8ed9bc3cc90e81c4c697d89
Mateo Nares <mateo.nares@gmail.com>
2025-11-11 15:05:25 -0800

    chore(styles): added typography (#6)
    added typography to main style sheet, components, and storybook
    
    Co-authored-by: Mateo Nares <mateonares@MacNerd.local>

--- src/components/ui/table/table.tsx
+++ src/components/ui/table/table.tsx
@@ -10,7 +10,7 @@ function Table({ className, ...props }: React.ComponentProps<'table'>) {
     >
       <table
         data-slot="table"
-        className={cn('w-full caption-bottom text-sm', className)}
+        className={cn('w-full caption-bottom text-body font-medium', className)}
         {...props}
       />
     </div>
@@ -68,7 +68,7 @@ function TableHead({ className, ...props }: React.ComponentProps<'th'>) {
     <th
       data-slot="table-head"
       className={cn(
-        'text-muted-foreground h-12 px-4 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
+        'text-body font-medium text-muted-foreground h-12 px-4 text-left align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
         className,
       )}
       {...props}
@@ -96,7 +96,7 @@ function TableCaption({
   return (
     <caption
       data-slot="table-caption"
-      className={cn('text-muted-foreground mt-4 text-sm', className)}
+      className={cn('text-body-small font-medium text-muted-foreground mt-4', className)}
       {...props}
     />
   );
