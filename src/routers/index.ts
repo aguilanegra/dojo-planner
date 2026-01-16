23b0ee497712f7bb00ea6d817df81b3250506c85
Mateo Nares <mateo.nares@gmail.com>
2026-01-16 11:27:46 -0800

    feat(cleanup): remove Todo feature and fix unused dependencies
    removed Todo feature and fixed unused dependencies

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,13 +1,7 @@
 import { addMembership, changeMembership, create as createMember, listMembershipPlans, remove as removeMember, restore as restoreMember, updateLastAccessed, update as updateMember, updateContactInfo as updateMemberContactInfo } from './Member';
 import { list as listMembers } from './Members';
-import { create, edit, remove } from './Todo';
 
 export const router = {
-  todo: {
-    create,
-    edit,
-    remove,
-  },
   members: {
     list: listMembers,
   },
